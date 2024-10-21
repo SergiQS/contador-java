291e48be24154a15c50b3e93a70eed62b1c7e37f
SergiQS <squetglass@gmail.com>
2024-10-21 20:35:25 +0200

    contador.java

--- contador.java
+++ contador.java
@@ -1,7 +1,7 @@
 public class contador{
 
 public static void main (String [] args) {
-    for (int i=1; i<=10; i++) {
+    for (int i=1; i<=20; i++) {
         System.out.println("Número"+i);
     }
 	
